3d80bb6692b49ae5d32639f849f8fcdad989b506
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2022-07-27 06:43:06 +0200

    Default port, no output of connect URL

--- src/MQTTClient.ts
+++ src/MQTTClient.ts
@@ -3,11 +3,11 @@ import { metrics } from "./MetricsModel"
 
 const user = process.env.MQTT_USER
 const pwd = process.env.MQTT_PASSWORD
-const server = process.env.MQTT_SERVER
-const port = process.env.MQTT_PORT
+const server = process.env.MQTT_SERVER || "localhost"
+const port = process.env.MQTT_PORT || "1883"
+const auth = user && pwd ? `${user}:${pwd}@` : ""
 
-console.log(`mqtt://${user}:${pwd}@${server}:${port}`)
-const client = mqtt.connect(`mqtt://${user}:${pwd}@${server}:${port}`)
+const client = mqtt.connect(`mqtt://${auth}${server}:${port}`)
 client.on("connect", () => {
   console.log("Connection established")
   client.subscribe("tele/tasmota_D9740F/SENSOR", (err) => {
