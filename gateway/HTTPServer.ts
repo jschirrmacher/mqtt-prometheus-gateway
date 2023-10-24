5be731db6361e2eb4428d99617208cc889abdace
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2023-10-24 19:43:51 +0200

    chore: use Server from useful-typescript-function

--- gateway/HTTPServer.ts
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-const HTTP_PORT = process.env.HTTP_PORT || 3000
-
-export const httpServer = express()
-
-httpServer.listen(HTTP_PORT, () => {
-  console.info("HTTP server tunning on http://localhost:" + HTTP_PORT)
-})
--- gateway/index.ts
+++ gateway/index.ts
@@ -1,15 +1,19 @@
 import { config } from "dotenv-flow"
 config()
 import readConfig from "./Config"
-import { httpServer } from "./HTTPServer"
 import getMetrics from "./MetricsGenerator"
 import setupMQTTClient from "./MQTTClient"
+import { routerBuilder, setupServer } from "useful-typescript-functions"
 
 async function setup() {
   const config = await readConfig()
   setupMQTTClient(config)
-  httpServer.get("/metrics", async (req, res) => {
-    res.header("Content-Type", "text/plain").send(getMetrics(config))
+  setupServer({
+    middlewares: [
+      routerBuilder("/metrics", "metrics")
+        .get("/", (req) => getMetrics(config))
+        .build(),
+    ],
   })
 }
 
