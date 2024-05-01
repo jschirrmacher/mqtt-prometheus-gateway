be156b788d668190285caf3bcfb773657cc13311
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2024-05-01 16:56:50 +0200

    fix: adapt to new version of useful-typescript-functions

--- gateway/index.ts
+++ gateway/index.ts
@@ -3,16 +3,14 @@ config()
 import readConfig from "./Config"
 import getMetrics from "./MetricsGenerator"
 import setupMQTTClient from "./MQTTClient"
-import { routerBuilder, setupServer } from "useful-typescript-functions"
+import { setupServer, defineRouter } from "useful-typescript-functions"
 
 async function setup() {
   const config = await readConfig()
   setupMQTTClient(config)
   setupServer({
-    middlewares: [
-      routerBuilder("/metrics", "metrics")
-        .get("/", (req) => getMetrics(config))
-        .build(),
+    routers: [
+      defineRouter("/metrics", "metrics").get("/", () => getMetrics(config)),
     ],
   })
 }
